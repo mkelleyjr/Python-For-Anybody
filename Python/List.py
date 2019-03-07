58028b425e37efc6a65ebe4fa271e937e1547d0a
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-06 22:27:58 -0500

    Basic look at lists in Python

---
+++ Python/List.py
@@ -0,0 +1,16 @@
+# Michael L. Kelley Jr.
+# March 6, 2019
+# Lists in Python
+
+# Lists hold information and can hold values like integers and strings
+
+# Make a simple list of colors and then print them out
+colors = ['blue', 'green', 'red', 'purple', 'yellow', 'black']
+print(colors)
+
+# You can also pick out specific elements of a list. Let's access purple
+# Note: items in a list start at 0, therefore, purple would be spot # 3 in the list
+print(colors[3])
+
+# More advanced topics include adding, deleting, and manipulating elements in a list. 
+# Look up more and try to see what you can do. 
