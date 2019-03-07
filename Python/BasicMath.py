ee1abfba90ef8e67160f8f2355da454a59777d09
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-06 22:01:24 -0500

    Basic math in python with examples

---
+++ Python/BasicMath.py
@@ -0,0 +1,22 @@
+# Michael L. Kelley Jr.
+# March 6, 2019
+# Basic math in Python
+
+# Display messages on how to do basic math problems in Python
+# Following each example will be a line of code showing it in action
+
+# Addition
+print("To add in python: print(1+1)")
+print(1+1)
+
+# Subtraction 
+print("To subtract in python: print(5-3)")
+print(5-3)
+
+# Multiplication 
+print("To multiply in Python: print(2*1)")
+print(2*1)
+
+# Division 
+print("To divide in Python: print(4/2)")
+print(4/2)
