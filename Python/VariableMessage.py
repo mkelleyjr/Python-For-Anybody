165e68eed540f5dd8ba02fc46f5d0b1ef8c4928e
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-07 13:12:33 -0500

    Added string variable example

---
+++ Python/VariableMessage.py
@@ -0,0 +1,10 @@
+# Michael L. Kelley Jr.
+# March 7, 2019
+# Variable message with Python
+
+# Assign a string message to a variable
+HelloMessage = "Hello, this is a variable message in Python"
+
+# Print the message to the console 
+print(HelloMessage)
+
