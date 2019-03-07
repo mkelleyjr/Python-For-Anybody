19ec0f3eec2848c922345fbb55b05a811f0b0649
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-06 21:28:21 -0500

    Added in BasicAddition.py file

---
+++ Python/BasicAddition.py
@@ -0,0 +1,16 @@
+# Michael L. Kelley Jr.
+# March 6, 2019
+# Math example program in Python
+
+# variables are used to hold data. Let's assign a few
+NumberOne = 1
+NumberTwo = 1
+
+# Add NumberOne and NumberTwo together to make NumberThree
+NumberThree = NumberOne + NumberTwo
+
+# Tell user what is happening
+print("NumberOne = 1, NumberTwo = 1")
+print("Adding NumberOne + NumberTwo:")
+print("NumberThree = ")
+print(NumberThree) 
