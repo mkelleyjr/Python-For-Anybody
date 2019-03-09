c6f2260308b761b5291986aad4a9f9bbf826e190
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-09 10:10:41 -0500

    Random number exmaple

---
+++ Python/RandomNumber.py
@@ -0,0 +1,12 @@
+# Michael L. Kelley Jr. 
+# March 9, 2019
+# RandomNumber.py
+# Random number example in Python
+
+# Import the random lib
+import random
+
+# Generate a random int between 1 - 100
+RandomNum = random.randint(1, 100)
+
+print(RandomNum)
