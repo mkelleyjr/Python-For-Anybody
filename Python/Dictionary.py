14d8561fca7882cc81f524efecbb482d3e435ef5
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-10 12:28:37 -0400

    Added dictionary example

---
+++ Python/Dictionary.py
@@ -0,0 +1,16 @@
+# Michael L. Kelley Jr.
+# March 10, 2019
+# Dictionary.py
+# Dictory example in Python
+
+# Define a dictionary in python and fill it with values 
+PyDict = {
+
+    "Brand": "Chevy",
+    "Model": "Corvette",
+    "Year": "1984",
+    "Color": "Red"
+}
+
+# Print the dictionary to the console 
+print(PyDict)
