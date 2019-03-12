0c83d9969e0cc51b269f05ff7f0ea42e138d92fa
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-11 20:23:49 -0400

    Added specific list element example

---
+++ Python/SpecificListValue.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# March 11, 2019
+# SpecificListValue.py
+
+# Use a specific element from a Python SpecificListValue
+
+# Build our list
+Colors = ['blue', 'green', 'red', 'yellow', 'black', 'purple']
+
+#Pull out a specific value in our list to use 
+Message = "My favorite color is " + Colors[0].title() + "."
+
+print(Message)
