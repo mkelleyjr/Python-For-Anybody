537721f65a8bdfcb36ced1c3093b87fac74e6863
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-12 14:34:00 -0400

    F String example

---
+++ Python/FString.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# March 12, 2019
+# FString.py
+
+# An example of F strings in Python
+# F String = "formatted string literal" 
+# F strings are easy to read with variables being inside {}
+
+Name = "Michael"
+Age = 33
+
+# Use f and {} for the f strings
+f"Hello, {Name}. You are {Age}."
---
+++ Python/ListReverse.py
@@ -0,0 +1,15 @@
+# Michael L. Kelley Jr.
+# March 12, 2019
+# ListReverse.py
+# Make a list and show how to reverse the contents
+
+# Make a list with some values
+MyList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(MyList)
+
+# Now, let's reverse the list 
+# Using the reverse() method
+MyList.reverse()
+
+print(MyList)
