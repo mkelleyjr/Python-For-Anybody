7b18674e2fb015f5f24dc756a7b35a7d28cbe340
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-13 10:52:45 -0400

    Boolean example

---
+++ Python/Boolean.py
@@ -0,0 +1,19 @@
+# Michael L. Kelley Jr.
+# March 13, 2019
+# Boolean.py
+
+# Comparing boolean values in Python
+# Try to run these in the interpreter 
+
+5 > 1
+# True
+2 > 27
+# False
+1 == 1
+# True
+100 > 10
+# True 
+25 >= 25
+# True
+6 != 3
+# True
--- Python/FString.py
+++ Python/FString.py
@@ -5,9 +5,14 @@
 # An example of F strings in Python
 # F String = "formatted string literal" 
 # F strings are easy to read with variables being inside {}
+# F strings are evaluated at runtime
 
 Name = "Michael"
 Age = 33
 
 # Use f and {} for the f strings
-f"Hello, {Name}. You are {Age}."
+# f or F can be used. Both are valid
+print(f"Hello, {Name}. You are {Age}.")
+
+# F string math example
+print(f"{5*5}")
