65a5ca289f363f6e2b768ab2e2d4ede8b65474d1
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-13 11:12:11 -0400

    String replication example

---
+++ Python/StringReplication.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# March 13, 2019
+# StringReplication.py
+
+# See how the * operator works with strings
+
+# This is what we are used to with integers
+print(5 * 5)
+# 25
+
+# This is what happens when we use it with a string
+print("Michael" * 5)
+# What happens when we do that???
