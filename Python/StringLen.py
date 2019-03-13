8f1079bcfec65f1f0efa956e9950e41e63045bda
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-13 10:59:54 -0400

    String length example

---
+++ Python/StringLen.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# March 13, 2019
+# StringLen.py
+
+# Find the length of a string in Python using len()
+
+
+MyString = "Michael"
+
+# Use the built-in len() function on this string
+StringLength = len(MyString)
+
+print(StringLength)
