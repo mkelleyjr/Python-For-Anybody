8ca16b216e61114546425bb3cd1e9dfecee9b117
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-14 22:57:06 -0400

    Minutes in x weeks sample exercise

---
+++ Python/MinsInWeek.py
@@ -0,0 +1,17 @@
+# Michael L. Kelley Jr.
+# March 14, 2019
+# MinsInWeek.py
+
+# Calculate how many minutes are in 7 weeks in Python
+# Sample exercise
+
+# Initiate variables to use
+
+MinsHour = 60
+HoursDay = 24
+DaysWeek = 7
+
+# Calculate the solution 
+Solution = (MinsHour * HoursDay) * (DaysWeek * 7)
+
+print(Solution)
