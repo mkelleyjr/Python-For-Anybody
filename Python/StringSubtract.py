17e34baf2927f46b2402fcd601f51430bff6befe
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-15 22:33:10 -0400

    String subtraction example

---
+++ Python/StringSubtract.py
@@ -0,0 +1,14 @@
+# Michael L. Kelley Jr.
+# March 15, 2019
+# StringSubtract.py
+
+# What happens when we use subtraction on a string?
+
+MyString = "Michael"
+
+# Do -2 on my name and see what happens
+LastLetter = MyString[-2]
+
+print(LastLetter)
+
+#Starting at the end of the string, it goes back 2 spots
