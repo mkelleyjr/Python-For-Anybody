9cf169f7e05835380810699b47ff4a957636e9f8
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-16 13:05:43 -0400

    print 0-100 example

---
+++ Python/Print100.py
@@ -0,0 +1,12 @@
+# Michael L. Kelley Jr.
+# March 16, 2019
+# Print100.py
+
+# Print 0-100 in Python using a for loop
+
+# Basic setup of a for loop 
+# Remember that the index starts at 0 
+for x in range(101):
+    print(x)
+
+    
