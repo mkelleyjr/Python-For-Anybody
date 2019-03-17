2d644dedcd60f7dd08c7e60216d8368852d46279
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-17 19:09:39 -0400

    User input example

---
+++ Python/UserInput.py
@@ -0,0 +1,14 @@
+# Michael L. Kelley Jr.
+# March 17, 2019
+# UserInput.py
+
+# Sample of how to get user input in Python
+# Take in a user string and display their input
+
+print("Please enter a string")
+
+# Hold their input in a variable 
+UserString = input()
+
+# Display their input 
+print("You entered " + UserString)
