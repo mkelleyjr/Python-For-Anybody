bd7a777fcffbe0411ff3610e463f0d79d782eaf0
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-19 19:17:08 -0400

    Writing to a file in Python!

---
+++ Python/FileWrite.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# March 19, 2019
+# FileWrite.py
+
+# Writing to a file is fast and simple in Python
+
+# Write a new file named MyFile. Use "w" for new files
+F = open("MyFile.txt", "w")
+
+# Write a message in our new file 
+F.write("This is writing in our MyFile.txt file!")
+
+
