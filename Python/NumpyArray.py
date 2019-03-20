e3f8744f622abffd534c965cb8d3dc025de1dc94
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-20 12:42:01 -0400

    Numpy Array Example

---
+++ Python/NumpyArray.py
@@ -0,0 +1,18 @@
+# Michael L. Kelley Jr.
+# March 20, 2019
+# NumpyArray.py
+
+# Using Numpy to make an Array
+
+#Use numpy || pip install numpy 
+import numpy as np
+
+# Define the array
+Array = np.array([1, 2, 3, 4, 5])
+
+print(Array)
+
+# Print the data type 
+print(Array.dtype)
+
+
