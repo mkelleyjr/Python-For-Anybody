4da416bb8edd9503bf4c87ce97ddf51a58b615d0
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-21 08:02:06 -0400

    Loop Breaking example

---
+++ Python/LoopBreak.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# March 21, 2019
+# LoopBreak.py
+
+# Break out of a for loop in Python
+
+# Make a loop through a string and stop once a certain variable is encountered
+# Here we check to see if x equals "a" and if it does, the loop is to break meaning it will stop
+for x in "Michael":
+    print(x)
+    if x == "a":
+        break
+
