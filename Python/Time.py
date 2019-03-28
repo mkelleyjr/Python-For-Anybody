9d13cc6838d5c145e9f89b36a4ac05160465a87e
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2019-03-28 09:55:55 -0400

    Getting time in python

---
+++ Python/Time.py
@@ -0,0 +1,14 @@
+# Michael L. Kelley Jr.
+# March 28, 2019
+# Time.py
+
+# Get the current time in Python! 
+
+import time 
+
+# Get the time
+current_time = time.localtime()
+
+# Printing will show you the actual variables in the object 
+print(current_time)
+
