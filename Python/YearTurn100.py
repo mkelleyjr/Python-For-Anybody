4f24bb4ee38cfafbc2b0a6746570d76c226bc3f3
mkelleyjr <michael.kelley.jr@gmail.com>
2020-05-02 02:51:03 -0400

    Added a new file

---
+++ Python/YearTurn100.py
@@ -0,0 +1,12 @@
+# Michael L. Kelley Jr.
+# May 2, 2020
+# YearTurn100.py
+# Ask user their age / tell them when they will be 100
+
+
+name = input("Hello, what is your name?")
+
+age = input("How old are you:")
+
+
+
